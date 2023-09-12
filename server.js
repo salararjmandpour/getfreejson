9fe7590d62e6c487da7a72d6b37cfe1d18e412d5
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-09-12 12:39:28 +0330

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-require("app-module-path").addPath(__dirname);
-const app = require("./app/index");
-require("dotenv").config();
-global.config = require("./app/config");
-
-//>---------------------- Start App
-
-new app();
