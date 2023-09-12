d2e0e29adcdbf53d96de5bac320174ef6c919921
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-09-12 12:37:16 +0330

    Delete app directory

--- app/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  port: 3004,
-  cookie_secretkey: process.env.COOKIE_SECRETKEY,
-  apiKey: process.env.weatherApiKey,
-  apiAddress: process.env.weatherApiAddress,
-};
--- app/http/controllers/getAll.js
+++
@@ -1,9 +0,0 @@
-const getAll = (req, res) => {
-
-     return res.redirect("https://jsondoc.iran.liara.run/");
-}
-
-
-//*>----------- module export
-
-module.exports = { getAll };
